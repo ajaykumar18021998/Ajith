374c35196b25cefd253f549f746caed7bddb1511
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-21 11:39:24 +0530

    Update aja.c

--- aja.c
+++ aja.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    char c;
+    char b;
     printf("Enter a character: ");
     scanf("%c",&c);
-    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
+    if( (b>='a' && b<='z') || (b>='A' && b<='Z'))
+        printf("%c is an alphabet.",b);
     else
-        printf("%c is not an alphabet.",c);
+        printf("%c is not an alphabet.",b);
     return 0;
 }
