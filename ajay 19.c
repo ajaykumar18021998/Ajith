237dc654d6b23dd2b3ab0c0c47714afbf8eef42c
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-21 12:23:01 +0530

    Create ajay 19.c

---
+++ ajay 19.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main()
+{
+inta,n2,i,temp,number,rem;
+printf("Enter two number(intervals):");
+scanf("%d%d",&n1&n2);
+printf("Amstrong number between% an %d are:"n1,n2);
+for(i=a+;i<n2;++1);
+{
+ temp+i;
+ number=0;
+ while (temp!=0)
+ {
+ rem=(temp%10);
+ num+=rem*rem*rem;
+ temp 1=10;
+ }
+ if(i==num)
+ {
+ printf("%d",i);
+ }
+ }
+ return 0;
+}
