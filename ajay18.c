0e85a06de3839c8a2992459aa8a259b015cfdb52
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-21 12:21:29 +0530

    Rename ajay17.c to ajay18.c

---
+++ ajay18.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number,original number,remainder,result=0;
+printf("enter an n integer:");
+scanf("%e",&num);
+original number=number;
+while(original number!=0)
+[
+remainder=original number%10;
+result=remainder*remainder*original number/=10;
+}
+if(result==number)
+printf("%e is an armstrong number",number);
+else
+printf("%e is not an armstrong number",number);
+return0;
+}
+
+    
