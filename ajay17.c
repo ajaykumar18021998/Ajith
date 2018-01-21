0d59dfa1c8a59f62a57f3e8ef13c4cfe334f7c06
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-21 12:17:28 +0530

    Create ajay17.c

---
+++ ajay17.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int a,result=0,rem;
+printf("enter the number");
+scanf("%d",&num);
+while(a!=0)
+{
+rem=a%10;
+result=result+rem+rem*rem
+num=a%10;
+}
+if(result=a)
+{
+printf("the number is armstrong");
+}
+else
+{
+printf("the number is not armstrong");
+}
+}
+
+ 
