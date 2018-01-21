ee3010cc1cf02be61eabc1ee1e794ccdf66f944b
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-21 10:43:41 +0530

    Create alpha r not.c

---
+++ alpha r not.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char b;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+  if( (b>='a' && b<='z') || (b>='A' && b<='Z'))
+     printf("%c is an alphabet.",b);
+    else
+    printf("%c is not an alphabet.",b);
+    return 0;
+}
