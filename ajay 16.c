4b7d1d10c0d0a6314e253e33ed91fc6d64ae4229
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-21 12:15:09 +0530

    Create ajay 16.c

---
+++ ajay 16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int c,b,i,s=0;
+	scanf("%d%d",&a,&b);
+	while(c<b)
+	{
+	for(i=2;i<c/2;i++)
+	{
+	if(c2==0)
+	{
+		s++;
+		break;
+	}
+	if(s==0)
+{
+	printf("%d",a);
+	a++;
+}
+}
+}
+	return 0;
+}
