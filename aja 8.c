72bbbb8a9baf21ff038ffa71c31534d90bc8c990
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-21 11:50:19 +0530

    Create aja 8.c

---
+++ aja 8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,n,sum=0;
+printf("\n enter the natural number");
+scanf("%d",&n);
+for(a=1;a<=n;a++)
+{
+sum +=a;
+printf("\n the natural number is %d"sum);
+}
+return 0;
+}
