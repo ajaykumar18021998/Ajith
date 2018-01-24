316b447d14f72ae80279c77a2be26386d0a18abe
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-24 16:37:13 +0530

    Create ajay29.c

---
+++ ajay29.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int sce,hour,m;
+printf("enter the time in sce");
+scanf("%d",&min);
+hour=min/60;
+m=min%60;
+printf("the hour and minutes are %d %d",hour,m);
+return(0);
+    }
