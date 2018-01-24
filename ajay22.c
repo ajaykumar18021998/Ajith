a0874bb0aed1ed0762811151fceaaa7be41dfdf9
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-24 15:51:40 +0530

    Rename ajay21.c to ajay22.c

---
+++ ajay22.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,v;
+int arr[10];
+printf("enter the total number ");
+scanf("%d",&v);
+for(i=0;i<v;++i)
+{
+printf("enter the number %d";i+1)
+scanf("%d",&arr[i]);
+for(i=1;i<v;++i)
+{
+if(arr[0]<arr[a])
+arr[0]=arr[a];
+}
+printf("maximum element=%d",arr[a]);
+}
+}
