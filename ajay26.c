f007bffc20b71b890850ae9711892fdcde49e727
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-24 16:03:43 +0530

    Rename ajay25.c to ajay26.c

---
+++ ajay26.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a[100],b,j,d=4,m=2,temp;
+for(i=0;i<d;i++)
+{
+scanf("%d",&a[i]);
+printf("%d",b);
+}
+for(j=0;j<m;j++)
+if(a[i]<a[j])
+{
+temp=a[b];
+a[i]=a[j];
+a[j]=temp;
+}
+printf("%d",a[i]);
+}
