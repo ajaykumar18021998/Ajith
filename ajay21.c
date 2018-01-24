c29a4759ce866f3fe4f1f3180aba564ceea7c61d
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-24 15:40:25 +0530

    Create ajay21.c

---
+++ ajay21.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int s,n,d,sum;
+printf("Enter the numbers");
+scanf("%d",&n);
+printf("Enter the starting number");
+scanf("%d",&s);
+printf("Enter the difference");
+scanf("%d",&d);
+sum=s+n+d;
+printf("%d",sum);
+}
