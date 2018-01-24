81fc55cbfe9c7394de63b11973e527af5dc10d31
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-24 15:36:42 +0530

    Create ajay20.c

---
+++ ajay20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num,a=1;
+printf("Enter any number:");
+scanf("%d",&num);
+printf("Multiplication table of%d:",num);
+while(a<=10)
+{
+printf("%dx%d=%d",num,a,num*i);
+i++;
+}
+return 0;
+}
+
