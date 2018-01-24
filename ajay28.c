1428a3eeee6cd9da44274c1b2b19b1cd427333f5
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-01-24 16:34:35 +0530

    Create ajay28.c

---
+++ ajay28.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int d[20],r,t;
+printf("Enter the size of array");
+scanf("%d",&d[i]);
+printf("Enter the elements");
+scanf("%d",&t);
+for(r=0;r<t;r++);
+{
+printf("%d%d",d[i],r);
+}
+}
