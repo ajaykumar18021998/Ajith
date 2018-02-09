3ebdc27695e502bf1027e9f2571a6f88e88add37
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-02-09 16:23:43 +0530

    Create ajay 40.c

---
+++ ajay 40.c
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,a,f=1,c=0;
+cout<<"enter the num";
+cin>>f;
+for(i=0;i<=5;i++)
+{
+  cout<<f;
+  f=f+c;
+  c=f;}
+return 0;
+getch();
+}
