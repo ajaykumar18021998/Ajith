07027d2064de03782501116977b6b7f27ff20ff4
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-02-28 16:11:21 +0530

    Rename ajay47.c to ajay 47.c

---
+++ ajay 47.c
@@ -0,0 +1,13 @@
+#iclude<stdio.h>
+void main() 
+{
+int k,y,sum=0,num;
+   printf(" Enter How many Number you want");
+  scanf("%d",&x);
+   printf(" Enter the elements one by one");
+  for(i=0;i<y;++i)
+   {
+     scanf("%d",&num);
+     Sum = Sum +num;
+   return 0;
+}
