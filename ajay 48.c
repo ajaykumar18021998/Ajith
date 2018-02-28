2ceb96b0326e05475c39af8e53f57ac4867c8f43
ajaykumar18021998 <31499364+ajaykumar18021998@users.noreply.github.com>
2018-02-28 15:45:33 +0530

    Rename ajay 38.c to ajay 48.c

---
+++ ajay 48.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+	int n=4,temp=0;
+	int a[]={'2','6','9','11'};
+	for(i=0;i<=3;i++)
+	{
+		for(j=1;j<=2;j++)
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+		printf("%d",a[0]);
+			}
+	printf("%d",a[2]);
+}
